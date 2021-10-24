f74d7f5b1dc55edd072e515560c2eef472885111
Dan Vittegleo <dan@vittegleo.com>
2021-10-24 11:46:06 -0700

    update to 12

--- hooks/aosp_build_pre.sh
+++ hooks/aosp_build_pre.sh
@@ -2,16 +2,11 @@
 
 cd "${AOSP_BUILD_DIR}"
 
-# apply microg sigspoof patch
-echo "applying microg sigspoof patch"
-patch -p1 --no-backup-if-mismatch < "platform/prebuilts/microg/00002-microg-sigspoof.patch"
-
-# apply community patches
-echo "applying community patch 00001-global-internet-permission-toggle.patch"
-community_patches_dir="${ROOT_DIR}/community_patches"
-rm -rf "${community_patches_dir}"
-git clone https://github.com/rattlesnakeos/community_patches "${community_patches_dir}"
-patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00001-global-internet-permission-toggle.patch"
+echo "applying microg patches"
+cd "${AOSP_BUILD_DIR}/packages/modules/Permission"
+patch -p1 --no-backup-if-mismatch < "${AOSP_BUILD_DIR}/platform/prebuilts/microg/00001-fake-package-sig.patch"
+cd "${AOSP_BUILD_DIR}/frameworks/base"
+patch -p1 --no-backup-if-mismatch < "${AOSP_BUILD_DIR}/platform/prebuilts/microg/00002-microg-sigspoof.patch"
 
 # apply custom hosts file
 custom_hosts_file="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
